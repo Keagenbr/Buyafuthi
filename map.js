14ab797fefa6fe742548c9d5a7e9664a2d48595d
Keagenbr <robeykb@gmail.com>
2021-07-23 16:17:17 +0200

    fixed map icon

--- map.js
+++ map.js
@@ -1,27 +1,24 @@
-
-
 function initMap() {
-  let pos = {lat: -29.83455582358241, lng: 31.00989884136558};
+  let pos = { lat: -29.83455582358241, lng: 31.00989884136558 };
 
   let options = {
     center: pos,
-    zoom:14,
+    zoom: 14,
   };
-
 
   let map = new google.maps.Map(document.getElementById("map"), options);
 
   let marker = new google.maps.Marker({
     position: pos,
     map: map,
-    icon: "Source/Buyafuti-map.svg"
+    icon: "assets/Buyafuti-map.svg",
   });
 
   let infoWindow = new google.maps.InfoWindow({
-    content: '<h1>Buyafuthi Activewear</h1>'
+    content: "<h1>Buyafuthi Activewear</h1>",
   });
 
-  marker.addListener('click', function(){
+  marker.addListener("click", function () {
     infoWindow.open(map, marker);
   });
 }
